3013d218d00ec333c76106cf197da1d9fb141d20
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2023-09-14 00:34:03 +0530

    Update 5_Comprison.js

--- Basics/5_Comprison.js
+++ Basics/5_Comprison.js
@@ -1,32 +1,32 @@
-/*console.log(2>1)  //out-->true
+console.log(2>1)  //out-->true
 console.log(2>=1)  //out-->true
 console.log(2<1)    //out-->false
 console.log(2==1)   //out-->false
 console.log(2!=1)   //out-->true
-*/
-/*
+
+
 console.log("2">1)  //out-->true
 console.log("01">1)  //out-->true
-*/
 
-/*
+
+
 console.log(null>0)  //out-->false
 console.log(null<0)  //out-->false
 console.log(null==0)  //out-->false
 console.log(null>=0)  //out-->true
-console.log(null<=0)  //out-->true  //note:-the reason is that an equality check == and comprisons >,<,>=,<= work differntly
+console.log(null<=0)  //out-->true  /* note:-the reason is that an equality check == and comprisons >,<,>=,<= work differntly
                                             Comprisons convert null to a number , treating it as 0.thats why null>=0 is true
                                             and null>0 is false
-*/
+                                      */
 
-/*
+
 console.log(undefined>0)  //out-->false
 console.log(undefined<0)  //out-->false
 console.log(undefined==0)  //out-->false
 console.log(undefined==0)  //out-->false
 console.log(undefined>=0)  //out-->false
 console.log(undefined<=0)  //out-->false
-*/
+
 
 console.log("2"===2) //out-->false  note:- it compare the value
 
