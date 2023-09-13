37443372c0dfef613ca6f3ce02420b4ffa499ec2
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2023-09-13 10:29:44 +0000

    DataTypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all javascript code as newer version
+//alert(3+3)//Error,because we are using node js, not browser
+console.log(3+3) /// output--6
+let name="prem"
+let age=18
+let isloged=false
+let state
+/*number=2 to the power 53
+bigint
+string-->" "
+boolean-->true/false
+null--->standalone value
+undefined-->
+symbol-->unique  */
+
+console.log(typeof "prem") // out-string
+console.log(typeof null)   // out-object
+console.log(typeof undifined) //out- undifined
+console.log(state) //out-->undifined
+console.table([name,age,isloged,state])
