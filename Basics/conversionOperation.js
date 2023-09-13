a6e08746ea74395dfa4ed5c6fa7a56a93c9b2a20
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2023-09-13 11:12:25 +0000

    conversionOperation

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,74 @@
+/*let score=33
+console.log(typeof score)   // out--> number
+        or
+console.log(typeof(score))  // out-->number
+*/
+/*let score="33"
+console.log(typeof score)   // out--> string
+*/
+
+/*let score="33"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(score)  //out-->33
+*/
+
+/*let score="33abc"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(valueInNumber)        //out-->NaN
+*/
+
+/*let score=null
+console.log(typeof score)   //out-->object
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(valueInNumber)        //out-->0
+*/
+
+/*let score=undifined
+console.log(typeof score)   //out-->undifined
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(valueInNumber)        //out-->NaN
+*/
+
+/*let score=true
+console.log(typeof score)   //out-->boolean
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(valueInNumber)        //out-->1           note:--> false=0,true=1 in number.
+*/
+
+/*let score="prem"
+console.log(typeof score)   //out-->string
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)  //out-->number
+console.log(valueInNumber)        //out-->NaN
+*/
+
+/*let isLogedIn=1
+let booleanIsLogedIn=Boolean(isLogedIn)
+console.log(booleanIsLogedIn)   //out-->true
+*/
+
+/*let isLogedIn=0
+let booleanIsLogedIn=Boolean(isLogedIn)
+console.log(booleanIsLogedIn)   //out-->false
+*/
+
+/*let isLogedIn=""
+let booleanIsLogedIn=Boolean(isLogedIn)
+console.log(booleanIsLogedIn)   //out-->false
+*/
+
+/*let isLogedIn="prem"
+let booleanIsLogedIn=Boolean(isLogedIn)
+console.log(booleanIsLogedIn)   //out-->true
+*/
+
+/*let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)    //out-->33
+console.log(typeof stringNumber)  //out-->string
+*/
