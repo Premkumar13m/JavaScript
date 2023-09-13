a6af7a75ae7cddcbc2d75fb11d187c39bf719ccb
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2023-09-13 12:23:33 +0000

    2nd commit

--- Basics/4_conversionOperation.js
+++ Basics/4_conversionOperation.js
@@ -72,3 +72,45 @@ let stringNumber=String(someNumber)
 console.log(stringNumber)    //out-->33
 console.log(typeof stringNumber)  //out-->string
 */
+
+//************Operation**********
+
+/*let value=3
+let negvalue=-value
+console.log(negvalue)  // out-->-3
+*/
+
+/*console.log(2+2)  //out-->4
+console.log(2-2)    //out-->0
+console.log(2*2)    //out-->4
+console.log(2**3)  //out-->8    (2 to the power , which is)
+console.log(2/3)   //out-->0.6666666666666666      (upto 16th place)
+console.log(2%3)   //out-->2
+*/
+
+/*let str1="hello"
+let str2='prem'
+console.log(str1+str2) //out-->helloprem
+*/
+
+/*console.log("1"+2);  //out-->12
+console.log(1+"2")   //out-->12
+console.log("1"+2+2)  //out-->122
+console.log(1+2+"2")  //out-->32
+*/
+/*console.log(true)    //out-->true
+console.log(+true)   //out-->1
+console.log(true+)  //out-->error
+console.log(+"")  //out-->0
+*/
+
+/*let num1,num2,num3
+num1=num2=num3=2+2
+console.table([num1,num2,num3])  //out-->4,4,4
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);   //out-->101
+*/
+ let gamecounter=100;
+ gamecounter++;
+ console.log(gamecounter)  //out-->101
