d1d6f0cd2e6650ce43e76b2b18a8cd24215aa373
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2024-01-21 17:00:21 +0000

    first_commit

--- Basics/12_Objects.js
+++ Basics/12_Objects.js
@@ -1,5 +1,5 @@
 //object literals
-const mySym=Symbol("key1")
+const mySym=Symbol("key1")   // this line  indicates that we are using the mySym Symbol as the key.
 
 const Juser={
     name:"prem" ,  //or  "name":"prem"
@@ -12,21 +12,21 @@ const Juser={
     lastLoginDays:["monday","saturrday"]
 }
 
-console.log(Juser.email)        //out-->pk@gmail.com
+// console.log(Juser.email)        //out-->pk@gmail.com
    //   or
-console.log(Juser["email"])     //out-->pk@gmail.com
+// console.log(Juser["email"])     //out-->pk@gmail.com
 
 
-console.log(Juser["full name"])  
+// console.log(Juser["full name"])  
 //console.log(Juser."full name")  or console.log(Juser.full name)   //wrong therefore always use Juser[] method.
 
-console.log(Juser[mySym])       //out-->mykey1    //note:--> we access symbol like this only its a rule.
-console.log(typeof mySym)       //out-->Symbol
+// console.log(Juser[mySym])       //out-->mykey1    //note:--> we access symbol like this only its a rule.
+// console.log(typeof mySym)       //out-->Symbol
 
-Juser["email"]="premkumar@gmail.com"      //note:- it will change the email value in the present Object.
-Object.freeze(Juser)
-Juser["email"]="premkumar2234@gmail.com"    //note:- it will not affect the email value because before this we have allready freeze the Juser.
-console.log(Juser)         
+// Juser["email"]="premkumar@gmail.com"      //note:- it will change the email value in the present Object.
+// Object.freeze(Juser)                      // the freeze() method should be commented while accessing the Juser from any function if it is not commented then the function will give the error.
+// Juser["email"]="premkumar2234@gmail.com"    //note:- it will not affect the email value because before this we have allready freeze the Juser.
+// console.log(Juser)         
 
 //out-->{
 //     name: 'prem',
@@ -40,7 +40,7 @@ console.log(Juser)
 //   }
 
 
-Juser.greeting=function(){
-    console.log(`hello js user ${this.name}`)   //out-->hello js user
+Juser.greetingTwo = function(){              //here we are creating the function called greetingTwo which access the Juser Object. 
+    console.log(`hello js user ${this.name}`)   //out-->hello js user prem
 }
-console.log(Juser.greeting())
+Juser.greetingTwo()        // here we are calling the greeting method.
